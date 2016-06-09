f5e6ae58d7bb732be421b8252eb8e0e53e30c341
abbyssoul <abbyssoul@gmail.com>
2016-06-09 23:50:29 +1000

    Refactored serial::enumeratePorts to use Path class

--- src/io/serial_list_ports.cpp
+++ src/io/serial_list_ports.cpp
@@ -24,44 +24,36 @@
 #include <tuple>
 #include <vector>
 #include <string>
-#include <sstream>
-#include <stdexcept>
 #include <iostream>
 #include <fstream>
-#include <cstdio>
-#include <cstdarg>
-#include <cstdlib>
+#include <memory>
 
-#include <glob.h>
-#include <sys/types.h>
 #include <sys/stat.h>
-#include <unistd.h>
-
-#include <fmt/format.h>
+#include <glob.h>
 
 
-using std::vector;
-using std::string;
+using Solace::String;
+using Solace::Path;
 
 
-vector<string> glob(std::initializer_list<const char*> patterns) {
-    vector<string> paths_found;
+const Path SYS_TTY_PATH = Path::parse("/sys/class/tty/");
+
+std::vector<Path> glob(std::initializer_list<const char*> patterns) {
+    std::vector<Path> paths_found;
 
     if (patterns.size() == 0)
         return paths_found;
 
     glob_t glob_results;
-//    int glob_retval =
     glob(*(patterns.begin()), 0, NULL, &glob_results);
 
-    std::initializer_list<const char*>::const_iterator iter = patterns.begin();
+    auto iter = patterns.begin();
     while (++iter != patterns.end()) {
-//        glob_retval =
         glob(*iter, GLOB_APPEND, NULL, &glob_results);
     }
 
     for (size_t path_index = 0; path_index < glob_results.gl_pathc; path_index++) {
-        paths_found.push_back(glob_results.gl_pathv[path_index]);
+        paths_found.push_back(Path::parse(glob_results.gl_pathv[path_index]));
     }
 
     globfree(&glob_results);
@@ -69,53 +61,28 @@ vector<string> glob(std::initializer_list<const char*> patterns) {
     return paths_found;
 }
 
-
-string basename(const string& path) {
-    const auto pos = path.rfind("/");
-
-    if (pos == std::string::npos)
-        return path;
-
-    return string(path, pos + 1, string::npos);
-}
-
-
-string dirname(const string& path) {
-    const auto pos = path.rfind("/");
-
-    if (pos == std::string::npos)
-        return path;
-
-    else if (pos == 0)
-        return "/";
-
-    return string(path, 0, pos);
-}
-
-
-bool path_exists(const string& path) {
+bool path_exists(const Path& path) {
     struct stat sb;
 
-    return (stat(path.c_str(), &sb ) == 0);
+    const auto& pathString = path.toString();
+    return (stat(pathString.c_str(), &sb ) == 0);
 }
 
-string realpath(const string& path) {
-    char* real_path = realpath(path.c_str(), NULL);
+Path realpath(const Path& path) {
+    const auto& pathString = path.toString();
+    std::unique_ptr<char, decltype(std::free)*> real_path{realpath(pathString.c_str(), NULL), std::free };
 
-    string result;
-    if (real_path != NULL) {
-        result = real_path;
-        free(real_path);
-    }
-
-    return result;
+    return (real_path)
+            ? Path::parse(String(real_path.get()))
+            : Path::Root;
 }
 
 
-string read_line(const string& file) {
-    std::ifstream ifs(file, std::ifstream::in);
+String read_line(const Path& file) {
+    const auto& pathString = file.toString();
+    std::ifstream ifs(pathString.to_str(), std::ifstream::in);
 
-    string line;
+    std::string line;
     if (ifs) {
         getline(ifs, line);
     }
@@ -124,61 +91,55 @@ string read_line(const string& file) {
 }
 
 
-string usb_sysfs_friendly_name(const string& sys_usb_path) {
-    uint device_number = 0;
-
-    std::istringstream(read_line(sys_usb_path + "/devnum")) >> device_number;
-
-    const string manufacturer = read_line(sys_usb_path + "/manufacturer");
-    const string product = read_line(sys_usb_path + "/product");
-    const string serial = read_line(sys_usb_path + "/serial");
+String usb_sysfs_friendly_name(const Path& sys_usb_path) {
+    const String manufacturer = read_line(sys_usb_path.join("manufacturer"));
+    const String product = read_line(sys_usb_path.join("product"));
+    const String serial = read_line(sys_usb_path.join("serial"));
 
     if (manufacturer.empty() && product.empty() && serial.empty())
-        return "";
+        return String::Empty;
 
-    // FIXME(abbyssoul): Should be String.join(" ", ...);
-    return fmt::format("{} {} {}", manufacturer, product, serial);
+    return String::join(" ", {manufacturer, product, serial});
 }
 
 
-string usb_sysfs_hw_string(const string& sysfs_path) {
-    string serial_number = read_line(sysfs_path + "/serial");
+String usb_sysfs_hw_string(const Path& sysfs_path) {
+    const auto vid = read_line(sysfs_path.join("idVendor"));
+    const auto pid = read_line(sysfs_path.join("idProduct"));
+    const auto serial_number = read_line(sysfs_path.join("serial"));
 
-    if (!serial_number.empty()) {
-        serial_number = fmt::format("SNR={}", serial_number);
-    }
-
-    string vid = read_line(sysfs_path + "/idVendor");
-    string pid = read_line(sysfs_path + "/idProduct");
-
-    return fmt::format("USB VID:PID={}:{} {}", vid, pid, serial_number);
+    return String("USB VID:PID=")
+            .concat(String::join(":", {vid, pid}))
+            .concat(serial_number.empty()
+                    ? String(" SNR=").concat(serial_number)
+                    : serial_number);
 }
 
 
-std::tuple<string, string> get_sysfs_info(const string& device_path) {
-    string friendly_name;
-    string hardware_id;
+std::tuple<String, String> get_sysfs_info(const Path& devicePath) {
+    String friendly_name;
+    String hardware_id;
 
-    const auto device_name = basename(device_path);
+    const auto device_name = devicePath.getBasename();
+    const auto sys_device_path = SYS_TTY_PATH.join(device_name).join("device");
 
-    string sys_device_path = fmt::format("/sys/class/tty/{}/device", device_name);
-    if (device_name.compare(0, 6, "ttyUSB") == 0) {
-        sys_device_path = dirname(dirname(realpath(sys_device_path)));
+    if (device_name.startsWith("ttyUSB")) {
+        const auto deviceSysPath = realpath(sys_device_path).getParent().getParent();
 
-        if (path_exists(sys_device_path)){
-            friendly_name = usb_sysfs_friendly_name(sys_device_path);
-            hardware_id = usb_sysfs_hw_string(sys_device_path);
+        if (path_exists(deviceSysPath)){
+            friendly_name = usb_sysfs_friendly_name(deviceSysPath);
+            hardware_id = usb_sysfs_hw_string(deviceSysPath);
         }
-    } else if (device_name.compare(0, 6, "ttyACM") == 0) {
-        sys_device_path = dirname(realpath(sys_device_path));
+    } else if (device_name.startsWith("ttyACM")) {
+        const auto deviceSysPath = realpath(sys_device_path).getParent();
 
-        if (path_exists(sys_device_path)) {
-            friendly_name = usb_sysfs_friendly_name(sys_device_path);
-            hardware_id = usb_sysfs_hw_string(sys_device_path);
+        if (path_exists(deviceSysPath)) {
+            friendly_name = usb_sysfs_friendly_name(deviceSysPath);
+            hardware_id = usb_sysfs_hw_string(deviceSysPath);
         }
     } else {
         // Try to read ID string of PCI device
-        string sys_id_path = sys_device_path + "/id";
+        const auto sys_id_path = sys_device_path.join("id");
         if (path_exists(sys_id_path))
             hardware_id = read_line(sys_id_path);
     }
@@ -195,7 +156,7 @@ std::tuple<string, string> get_sysfs_info(const string& device_path) {
 
 Solace::Array<Solace::IO::SerialPortInfo>
 Solace::IO::Serial::enumeratePorts() {
-    std::vector<std::string> devices_found = glob({
+    const auto devices_found = glob({
                                             "/dev/ttyACM*",
                                             "/dev/ttyS*",
                                             "/dev/ttyUSB*",
@@ -203,18 +164,15 @@ Solace::IO::Serial::enumeratePorts() {
                                             "/dev/cu.*"
                                         });
 
-    vector<Solace::IO::SerialPortInfo> results;
+    std::vector<Solace::IO::SerialPortInfo> results;
     results.reserve(devices_found.size());
 
     for (const auto& device : devices_found) {
-        string friendly_name;
-        string hardware_id;
+        String friendly_name;
+        String hardware_id;
         std::tie(friendly_name, hardware_id) = get_sysfs_info(device);
 
-        results.emplace_back(
-                    Solace::Path::parse(device),
-                    Solace::String(friendly_name),
-                    Solace::String(hardware_id));
+        results.emplace_back(device, friendly_name, hardware_id);
     }
 
     return { results };
