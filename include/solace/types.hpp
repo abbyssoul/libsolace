ec996d0ebb0f79e8b7409af60a7c07ef9c0099ff
abbyssoul <abbyssoul@gmail.com>
2017-10-01 22:39:42 +1100

    better clang compat and header only libfmt in test

--- include/solace/types.hpp
+++ include/solace/types.hpp
@@ -28,18 +28,18 @@
 #include "solace/libsolace_config.hpp"
 
 #include <cstddef>
-#include <cstdint>
+#include <stdint.h>
 
 namespace Solace {
 
-typedef std::int8_t			int8;
-typedef std::int16_t		int16;
-typedef std::int32_t		int32;
-typedef std::int64_t		int64;
-typedef std::uint8_t        uint8;
-typedef std::uint16_t       uint16;
-typedef std::uint32_t       uint32;
-typedef std::uint64_t 		uint64;
+typedef int8_t      int8;
+typedef int16_t		int16;
+typedef int32_t		int32;
+typedef int64_t		int64;
+typedef uint8_t     uint8;
+typedef uint16_t    uint16;
+typedef uint32_t    uint32;
+typedef uint64_t 	uint64;
 
 
 //! 32 bit floating point variable.
