73c14a3fa95e367c5fc8893d700a986988c62b40
abbyssoul <abbyssoul@gmail.com>
2017-10-23 00:50:20 +1100

    Codecheck complaints cleanup

--- src/hashing/murmur3.cpp
+++ src/hashing/murmur3.cpp
@@ -27,14 +27,14 @@ static const String MURMUR3_128_NAME = "MURMUR3-128";
 #define FORCE_INLINE	__forceinline
 #include <stdlib.h>
 
-#define ROTL32(x,y)	_rotl(x,y)
-#define ROTL64(x,y)	_rotl64(x,y)
+#define ROTL32(x, y)	_rotl(x, y)
+#define ROTL64(x, y)	_rotl64(x, y)
 
 #define BIG_CONSTANT(x) (x)
 
 // Other compilers
 
-#else	// defined(_MSC_VER)
+#else  // defined(_MSC_VER)
 
 #define	FORCE_INLINE inline __attribute__((always_inline))
 
@@ -46,12 +46,12 @@ inline uint64 rotl64(uint64 x, int8 r) {
     return (x << r) | (x >> (64 - r));
 }
 
-#define	ROTL32(x,y)	rotl32(x,y)
-#define ROTL64(x,y)	rotl64(x,y)
+#define	ROTL32(x, y)	rotl32(x, y)
+#define ROTL64(x, y)	rotl64(x, y)
 
 #define BIG_CONSTANT(x) (x##LLU)
 
-#endif // !defined(_MSC_VER)
+#endif  // !defined(_MSC_VER)
 
 //-----------------------------------------------------------------------------
 // Block read - if your platform needs to do endian-swapping or can only
@@ -103,11 +103,11 @@ murmurhash3_x86_32(const byte *data, uint32 len, uint32 seed) {
     uint32 h = 0;
     uint32 k = 0;
     int i = 0;
-    const int l = len / 4; // chunk length
+    const int l = len / 4;  // chunk length
 
     h = seed;
-    const uint32* chunks = reinterpret_cast<const uint32*>(data + l * 4); // body;
-    const uint8* tail = reinterpret_cast<const uint8*>(data + l * 4); // last 8 byte chunk of `key'; // tail - last 8 bytes
+    const uint32* chunks = reinterpret_cast<const uint32*>(data + l * 4);  // body
+    const uint8* tail = reinterpret_cast<const uint8*>(data + l * 4);  // last 8 byte chunk of `key'
 
     // for each 4 byte chunk of `key'
     for (i = -l; i != 0; ++i) {
@@ -128,9 +128,9 @@ murmurhash3_x86_32(const byte *data, uint32 len, uint32 seed) {
     k = 0;
 
     // remainder
-    switch (len & 3) { // `len % 4'
-    case 3: k ^= static_cast<uint32>(tail[2] << 16);
-    case 2: k ^= static_cast<uint32>(tail[1] << 8);
+    switch (len & 3) {  // `len % 4'
+    case 3: k ^= static_cast<uint32>(tail[2] << 16);  /* Falls through. */
+    case 2: k ^= static_cast<uint32>(tail[1] << 8);   /* Falls through. */
     case 1:
       k ^= tail[0];
       k *= c1;
@@ -180,21 +180,21 @@ void MurmurHash3_x86_128(const byte* data, const Murmur3_128::size_type len, uin
 //        getUint32_LE(k4, reinterpret_cast<const byte*>(&blocks[i*4 + 3]), 0);
 
 
-        k1 *= c1; k1  = ROTL32(k1,15); k1 *= c2; h1 ^= k1;
+        k1 *= c1; k1  = ROTL32(k1, 15); k1 *= c2; h1 ^= k1;
 
-        h1 = ROTL32(h1,19); h1 += h2; h1 = h1*5+0x561ccd1b;
+        h1 = ROTL32(h1, 19); h1 += h2; h1 = h1 * 5 + 0x561ccd1b;
 
-        k2 *= c2; k2  = ROTL32(k2,16); k2 *= c3; h2 ^= k2;
+        k2 *= c2; k2  = ROTL32(k2, 16); k2 *= c3; h2 ^= k2;
 
-        h2 = ROTL32(h2,17); h2 += h3; h2 = h2*5+0x0bcaa747;
+        h2 = ROTL32(h2, 17); h2 += h3; h2 = h2 * 5 + 0x0bcaa747;
 
-        k3 *= c3; k3  = ROTL32(k3,17); k3 *= c4; h3 ^= k3;
+        k3 *= c3; k3  = ROTL32(k3, 17); k3 *= c4; h3 ^= k3;
 
-        h3 = ROTL32(h3,15); h3 += h4; h3 = h3*5+0x96cd1c35;
+        h3 = ROTL32(h3, 15); h3 += h4; h3 = h3 * 5 + 0x96cd1c35;
 
-        k4 *= c4; k4  = ROTL32(k4,18); k4 *= c1; h4 ^= k4;
+        k4 *= c4; k4  = ROTL32(k4, 18); k4 *= c1; h4 ^= k4;
 
-        h4 = ROTL32(h4,13); h4 += h1; h4 = h4*5+0x32ac3b17;
+        h4 = ROTL32(h4, 13); h4 += h1; h4 = h4 * 5 + 0x32ac3b17;
     }
 
     //----------
@@ -208,29 +208,29 @@ void MurmurHash3_x86_128(const byte* data, const Murmur3_128::size_type len, uin
     uint32 k4 = 0;
 
     switch (len & 15) {
-    case 15: k4 ^= static_cast<uint32>(tail[14]) << 16;
-    case 14: k4 ^= static_cast<uint32>(tail[13]) << 8;
-    case 13: k4 ^= static_cast<uint32>(tail[12]) << 0;
-           k4 *= c4; k4  = ROTL32(k4,18); k4 *= c1; h4 ^= k4;
-
-    case 12: k3 ^= static_cast<uint32>(tail[11]) << 24;
-    case 11: k3 ^= static_cast<uint32>(tail[10]) << 16;
-    case 10: k3 ^= static_cast<uint32>(tail[ 9]) << 8;
-    case  9: k3 ^= static_cast<uint32>(tail[ 8]) << 0;
-           k3 *= c3; k3  = ROTL32(k3,17); k3 *= c4; h3 ^= k3;
-
-    case  8: k2 ^= static_cast<uint32>(tail[ 7]) << 24;
-    case  7: k2 ^= static_cast<uint32>(tail[ 6]) << 16;
-    case  6: k2 ^= static_cast<uint32>(tail[ 5]) << 8;
-    case  5: k2 ^= static_cast<uint32>(tail[ 4]) << 0;
-           k2 *= c2; k2  = ROTL32(k2,16); k2 *= c3; h2 ^= k2;
-
-    case  4: k1 ^= static_cast<uint32>(tail[ 3]) << 24;
-    case  3: k1 ^= static_cast<uint32>(tail[ 2]) << 16;
-    case  2: k1 ^= static_cast<uint32>(tail[ 1]) << 8;
-    case  1: k1 ^= static_cast<uint32>(tail[ 0]) << 0;
-           k1 *= c1; k1  = ROTL32(k1,15); k1 *= c2; h1 ^= k1;
-    };
+    case 15: k4 ^= static_cast<uint32>(tail[14]) << 16;  /* Falls through. */
+    case 14: k4 ^= static_cast<uint32>(tail[13]) << 8;   /* Falls through. */
+    case 13: k4 ^= static_cast<uint32>(tail[12]) << 0;   /* Falls through. */
+           k4 *= c4; k4  = ROTL32(k4, 18); k4 *= c1; h4 ^= k4;
+    /* Falls through. */
+    case 12: k3 ^= static_cast<uint32>(tail[11]) << 24;   /* Falls through. */
+    case 11: k3 ^= static_cast<uint32>(tail[10]) << 16;   /* Falls through. */
+    case 10: k3 ^= static_cast<uint32>(tail[ 9]) << 8;   /* Falls through. */
+    case  9: k3 ^= static_cast<uint32>(tail[ 8]) << 0;   /* Falls through. */
+           k3 *= c3; k3  = ROTL32(k3, 17); k3 *= c4; h3 ^= k3;
+    /* Falls through. */
+    case  8: k2 ^= static_cast<uint32>(tail[ 7]) << 24;   /* Falls through. */
+    case  7: k2 ^= static_cast<uint32>(tail[ 6]) << 16;   /* Falls through. */
+    case  6: k2 ^= static_cast<uint32>(tail[ 5]) << 8;   /* Falls through. */
+    case  5: k2 ^= static_cast<uint32>(tail[ 4]) << 0;   /* Falls through. */
+           k2 *= c2; k2  = ROTL32(k2, 16); k2 *= c3; h2 ^= k2;
+    /* Falls through. */
+    case  4: k1 ^= static_cast<uint32>(tail[ 3]) << 24;   /* Falls through. */
+    case  3: k1 ^= static_cast<uint32>(tail[ 2]) << 16;   /* Falls through. */
+    case  2: k1 ^= static_cast<uint32>(tail[ 1]) << 8;   /* Falls through. */
+    case  1: k1 ^= static_cast<uint32>(tail[ 0]) << 0;   /* Falls through. */
+           k1 *= c1; k1  = ROTL32(k1, 15); k1 *= c2; h1 ^= k1;
+    }
 
     //----------
     // finalization
@@ -276,13 +276,13 @@ void MurmurHash3_x64_128(const byte* data, const Murmur3_128::size_type len, uin
 //        getUint64_LE(k1, reinterpret_cast<const byte*>(&blocks[i*2 + 0]), 0);
 //        getUint64_LE(k2, reinterpret_cast<const byte*>(&blocks[i*2 + 1]), 0);
 
-        k1 *= c1; k1  = ROTL64(k1,31); k1 *= c2; h1 ^= k1;
+        k1 *= c1; k1  = ROTL64(k1, 31); k1 *= c2; h1 ^= k1;
 
-        h1 = ROTL64(h1,27); h1 += h2; h1 = h1*5+0x52dce729;
+        h1 = ROTL64(h1, 27); h1 += h2; h1 = h1*5+0x52dce729;
 
-        k2 *= c2; k2  = ROTL64(k2,33); k2 *= c1; h2 ^= k2;
+        k2 *= c2; k2  = ROTL64(k2, 33); k2 *= c1; h2 ^= k2;
 
-        h2 = ROTL64(h2,31); h2 += h1; h2 = h2*5+0x38495ab5;
+        h2 = ROTL64(h2, 31); h2 += h1; h2 = h2*5+0x38495ab5;
     }
 
     //----------
@@ -293,25 +293,25 @@ void MurmurHash3_x64_128(const byte* data, const Murmur3_128::size_type len, uin
     uint64 k2 = 0;
 
     switch (len & 15) {
-    case 15: k2 ^= static_cast<uint64>(tail[14]) << 48;
-    case 14: k2 ^= static_cast<uint64>(tail[13]) << 40;
-    case 13: k2 ^= static_cast<uint64>(tail[12]) << 32;
-    case 12: k2 ^= static_cast<uint64>(tail[11]) << 24;
-    case 11: k2 ^= static_cast<uint64>(tail[10]) << 16;
-    case 10: k2 ^= static_cast<uint64>(tail[ 9]) << 8;
-    case  9: k2 ^= static_cast<uint64>(tail[ 8]) << 0;
-           k2 *= c2; k2  = ROTL64(k2,33); k2 *= c1; h2 ^= k2;
-
-    case  8: k1 ^= static_cast<uint64>(tail[ 7]) << 56;
-    case  7: k1 ^= static_cast<uint64>(tail[ 6]) << 48;
-    case  6: k1 ^= static_cast<uint64>(tail[ 5]) << 40;
-    case  5: k1 ^= static_cast<uint64>(tail[ 4]) << 32;
-    case  4: k1 ^= static_cast<uint64>(tail[ 3]) << 24;
-    case  3: k1 ^= static_cast<uint64>(tail[ 2]) << 16;
-    case  2: k1 ^= static_cast<uint64>(tail[ 1]) << 8;
-    case  1: k1 ^= static_cast<uint64>(tail[ 0]) << 0;
-           k1 *= c1; k1  = ROTL64(k1,31); k1 *= c2; h1 ^= k1;
-    };
+    case 15: k2 ^= static_cast<uint64>(tail[14]) << 48; /* Falls through. */
+    case 14: k2 ^= static_cast<uint64>(tail[13]) << 40; /* Falls through. */
+    case 13: k2 ^= static_cast<uint64>(tail[12]) << 32; /* Falls through. */
+    case 12: k2 ^= static_cast<uint64>(tail[11]) << 24; /* Falls through. */
+    case 11: k2 ^= static_cast<uint64>(tail[10]) << 16; /* Falls through. */
+    case 10: k2 ^= static_cast<uint64>(tail[ 9]) << 8; /* Falls through. */
+    case  9: k2 ^= static_cast<uint64>(tail[ 8]) << 0; /* Falls through. */
+           k2 *= c2; k2  = ROTL64(k2, 33); k2 *= c1; h2 ^= k2;
+    /* Falls through. */
+    case  8: k1 ^= static_cast<uint64>(tail[ 7]) << 56; /* Falls through. */
+    case  7: k1 ^= static_cast<uint64>(tail[ 6]) << 48; /* Falls through. */
+    case  6: k1 ^= static_cast<uint64>(tail[ 5]) << 40; /* Falls through. */
+    case  5: k1 ^= static_cast<uint64>(tail[ 4]) << 32; /* Falls through. */
+    case  4: k1 ^= static_cast<uint64>(tail[ 3]) << 24; /* Falls through. */
+    case  3: k1 ^= static_cast<uint64>(tail[ 2]) << 16; /* Falls through. */
+    case  2: k1 ^= static_cast<uint64>(tail[ 1]) << 8; /* Falls through. */
+    case  1: k1 ^= static_cast<uint64>(tail[ 0]) << 0; /* Falls through. */
+           k1 *= c1; k1  = ROTL64(k1, 31); k1 *= c2; h1 ^= k1;
+    }
 
     //----------
     // finalization
--- src/hashing/sha1.cpp
+++ src/hashing/sha1.cpp
@@ -55,19 +55,17 @@ void sha1_process(Sha1::State& ctx, const byte data[64]) {
     getUint32_BE(W[14], data, 56);
     getUint32_BE(W[15], data, 60);
 
-#define S(x,n) ((x << n) | ((x & 0xFFFFFFFF) >> (32 - n)))
+#define S(x, n) ((x << n) | ((x & 0xFFFFFFFF) >> (32 - n)))
 
 #define R(t)                                            \
 (                                                       \
-    temp = W[( t -  3 ) & 0x0F] ^ W[( t - 8 ) & 0x0F] ^ \
-           W[( t - 14 ) & 0x0F] ^ W[  t       & 0x0F],  \
-    ( W[t & 0x0F] = S(temp,1) )                         \
+    temp = W[(t -  3) & 0x0F] ^ W[(t - 8) & 0x0F] ^ \
+           W[(t - 14) & 0x0F] ^ W[ t      & 0x0F],  \
+    (W[t & 0x0F] = S(temp, 1))                      \
 )
 
-#define P(a,b,c,d,e,x)                                  \
-{                                                       \
-    e += S(a,5) + F(b,c,d) + K + x; b = S(b,30);        \
-}
+#define P(a, b, c, d, e, x)                                  \
+{ e += S(a, 5) + F(b, c, d) + K + x; b = S(b, 30); }
 
     A = ctx.state[0];
     B = ctx.state[1];
@@ -75,110 +73,110 @@ void sha1_process(Sha1::State& ctx, const byte data[64]) {
     D = ctx.state[3];
     E = ctx.state[4];
 
-#define F(x,y,z) (z ^ (x & (y ^ z)))
+#define F(x, y, z) (z ^ (x & (y ^ z)))
 #define K 0x5A827999
 
-    P( A, B, C, D, E, W[0]  );
-    P( E, A, B, C, D, W[1]  );
-    P( D, E, A, B, C, W[2]  );
-    P( C, D, E, A, B, W[3]  );
-    P( B, C, D, E, A, W[4]  );
-    P( A, B, C, D, E, W[5]  );
-    P( E, A, B, C, D, W[6]  );
-    P( D, E, A, B, C, W[7]  );
-    P( C, D, E, A, B, W[8]  );
-    P( B, C, D, E, A, W[9]  );
-    P( A, B, C, D, E, W[10] );
-    P( E, A, B, C, D, W[11] );
-    P( D, E, A, B, C, W[12] );
-    P( C, D, E, A, B, W[13] );
-    P( B, C, D, E, A, W[14] );
-    P( A, B, C, D, E, W[15] );
-    P( E, A, B, C, D, R(16) );
-    P( D, E, A, B, C, R(17) );
-    P( C, D, E, A, B, R(18) );
-    P( B, C, D, E, A, R(19) );
+    P(A, B, C, D, E, W[0]);
+    P(E, A, B, C, D, W[1]);
+    P(D, E, A, B, C, W[2]);
+    P(C, D, E, A, B, W[3]);
+    P(B, C, D, E, A, W[4]);
+    P(A, B, C, D, E, W[5]);
+    P(E, A, B, C, D, W[6]);
+    P(D, E, A, B, C, W[7]);
+    P(C, D, E, A, B, W[8]);
+    P(B, C, D, E, A, W[9]);
+    P(A, B, C, D, E, W[10]);
+    P(E, A, B, C, D, W[11]);
+    P(D, E, A, B, C, W[12]);
+    P(C, D, E, A, B, W[13]);
+    P(B, C, D, E, A, W[14]);
+    P(A, B, C, D, E, W[15]);
+    P(E, A, B, C, D, R(16));
+    P(D, E, A, B, C, R(17));
+    P(C, D, E, A, B, R(18));
+    P(B, C, D, E, A, R(19));
 
 #undef K
 #undef F
 
-#define F(x,y,z) (x ^ y ^ z)
+#define F(x, y, z) (x ^ y ^ z)
 #define K 0x6ED9EBA1
 
-    P( A, B, C, D, E, R(20) );
-    P( E, A, B, C, D, R(21) );
-    P( D, E, A, B, C, R(22) );
-    P( C, D, E, A, B, R(23) );
-    P( B, C, D, E, A, R(24) );
-    P( A, B, C, D, E, R(25) );
-    P( E, A, B, C, D, R(26) );
-    P( D, E, A, B, C, R(27) );
-    P( C, D, E, A, B, R(28) );
-    P( B, C, D, E, A, R(29) );
-    P( A, B, C, D, E, R(30) );
-    P( E, A, B, C, D, R(31) );
-    P( D, E, A, B, C, R(32) );
-    P( C, D, E, A, B, R(33) );
-    P( B, C, D, E, A, R(34) );
-    P( A, B, C, D, E, R(35) );
-    P( E, A, B, C, D, R(36) );
-    P( D, E, A, B, C, R(37) );
-    P( C, D, E, A, B, R(38) );
-    P( B, C, D, E, A, R(39) );
+    P(A, B, C, D, E, R(20));
+    P(E, A, B, C, D, R(21));
+    P(D, E, A, B, C, R(22));
+    P(C, D, E, A, B, R(23));
+    P(B, C, D, E, A, R(24));
+    P(A, B, C, D, E, R(25));
+    P(E, A, B, C, D, R(26));
+    P(D, E, A, B, C, R(27));
+    P(C, D, E, A, B, R(28));
+    P(B, C, D, E, A, R(29));
+    P(A, B, C, D, E, R(30));
+    P(E, A, B, C, D, R(31));
+    P(D, E, A, B, C, R(32));
+    P(C, D, E, A, B, R(33));
+    P(B, C, D, E, A, R(34));
+    P(A, B, C, D, E, R(35));
+    P(E, A, B, C, D, R(36));
+    P(D, E, A, B, C, R(37));
+    P(C, D, E, A, B, R(38));
+    P(B, C, D, E, A, R(39));
 
 #undef K
 #undef F
 
-#define F(x,y,z) ((x & y) | (z & (x | y)))
+#define F(x, y, z) ((x & y) | (z & (x | y)))
 #define K 0x8F1BBCDC
 
-    P( A, B, C, D, E, R(40) );
-    P( E, A, B, C, D, R(41) );
-    P( D, E, A, B, C, R(42) );
-    P( C, D, E, A, B, R(43) );
-    P( B, C, D, E, A, R(44) );
-    P( A, B, C, D, E, R(45) );
-    P( E, A, B, C, D, R(46) );
-    P( D, E, A, B, C, R(47) );
-    P( C, D, E, A, B, R(48) );
-    P( B, C, D, E, A, R(49) );
-    P( A, B, C, D, E, R(50) );
-    P( E, A, B, C, D, R(51) );
-    P( D, E, A, B, C, R(52) );
-    P( C, D, E, A, B, R(53) );
-    P( B, C, D, E, A, R(54) );
-    P( A, B, C, D, E, R(55) );
-    P( E, A, B, C, D, R(56) );
-    P( D, E, A, B, C, R(57) );
-    P( C, D, E, A, B, R(58) );
-    P( B, C, D, E, A, R(59) );
+    P(A, B, C, D, E, R(40));
+    P(E, A, B, C, D, R(41));
+    P(D, E, A, B, C, R(42));
+    P(C, D, E, A, B, R(43));
+    P(B, C, D, E, A, R(44));
+    P(A, B, C, D, E, R(45));
+    P(E, A, B, C, D, R(46));
+    P(D, E, A, B, C, R(47));
+    P(C, D, E, A, B, R(48));
+    P(B, C, D, E, A, R(49));
+    P(A, B, C, D, E, R(50));
+    P(E, A, B, C, D, R(51));
+    P(D, E, A, B, C, R(52));
+    P(C, D, E, A, B, R(53));
+    P(B, C, D, E, A, R(54));
+    P(A, B, C, D, E, R(55));
+    P(E, A, B, C, D, R(56));
+    P(D, E, A, B, C, R(57));
+    P(C, D, E, A, B, R(58));
+    P(B, C, D, E, A, R(59));
 
 #undef K
 #undef F
 
-#define F(x,y,z) (x ^ y ^ z)
+#define F(x, y, z) (x ^ y ^ z)
 #define K 0xCA62C1D6
 
-    P( A, B, C, D, E, R(60) );
-    P( E, A, B, C, D, R(61) );
-    P( D, E, A, B, C, R(62) );
-    P( C, D, E, A, B, R(63) );
-    P( B, C, D, E, A, R(64) );
-    P( A, B, C, D, E, R(65) );
-    P( E, A, B, C, D, R(66) );
-    P( D, E, A, B, C, R(67) );
-    P( C, D, E, A, B, R(68) );
-    P( B, C, D, E, A, R(69) );
-    P( A, B, C, D, E, R(70) );
-    P( E, A, B, C, D, R(71) );
-    P( D, E, A, B, C, R(72) );
-    P( C, D, E, A, B, R(73) );
-    P( B, C, D, E, A, R(74) );
-    P( A, B, C, D, E, R(75) );
-    P( E, A, B, C, D, R(76) );
-    P( D, E, A, B, C, R(77) );
-    P( C, D, E, A, B, R(78) );
-    P( B, C, D, E, A, R(79) );
+    P(A, B, C, D, E, R(60));
+    P(E, A, B, C, D, R(61));
+    P(D, E, A, B, C, R(62));
+    P(C, D, E, A, B, R(63));
+    P(B, C, D, E, A, R(64));
+    P(A, B, C, D, E, R(65));
+    P(E, A, B, C, D, R(66));
+    P(D, E, A, B, C, R(67));
+    P(C, D, E, A, B, R(68));
+    P(B, C, D, E, A, R(69));
+    P(A, B, C, D, E, R(70));
+    P(E, A, B, C, D, R(71));
+    P(D, E, A, B, C, R(72));
+    P(C, D, E, A, B, R(73));
+    P(B, C, D, E, A, R(74));
+    P(A, B, C, D, E, R(75));
+    P(E, A, B, C, D, R(76));
+    P(D, E, A, B, C, R(77));
+    P(C, D, E, A, B, R(78));
+    P(B, C, D, E, A, R(79));
 
 #undef K
 #undef F
@@ -264,14 +262,14 @@ MessageDigest Sha1::digest() {
     uint32 high, low;
 
     byte msglen[8];
-    high = (_state.total[0] >> 29 ) | ( _state.total[1] <<  3 );
-    low  = (_state.total[0] <<  3 );
+    high = (_state.total[0] >> 29) | (_state.total[1] <<  3);
+    low  = (_state.total[0] <<  3);
 
-    putUint32_BE( high, msglen, 0 );
-    putUint32_BE( low,  msglen, 4 );
+    putUint32_BE(high, msglen, 0);
+    putUint32_BE(low,  msglen, 4);
 
     const uint32 last = _state.total[0] & 0x3F;
-    const uint32 padn = ( last < 56 ) ? ( 56 - last ) : ( 120 - last );
+    const uint32 padn = (last < 56) ? (56 - last) : (120 - last);
 
     sha1_update(_state, sha1_padding, padn);
     sha1_update(_state, msglen, 8);
