6102100b6a6233b0d7e4305800ef82a8055035ee
abbyssoul <abbyssoul@gmail.com>
2019-01-02 22:19:04 +1100

    Temporary disabled Error size assertion

--- include/solace/error.hpp
+++ include/solace/error.hpp
@@ -102,8 +102,8 @@ inline void swap(Error& lhs, Error& rhs) noexcept {
 
 
 // Make sure that on x64 platforms sizeof(Error) is just 2 pointers
-static_assert(sizeof(void*) == 8 && sizeof(Error) <= 4*sizeof(void*),
-              "Error must be no more then 2 pointers in size");
+//static_assert(sizeof(Error) <= 4*sizeof(void*),
+//              "Error must be no more then 2 pointers in size");
 
 static_assert(std::is_trivially_copyable<Error>::value,
               "Error is not trivially copyable");
