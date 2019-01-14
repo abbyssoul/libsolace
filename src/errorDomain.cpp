f2cf7ce934ac29a5cf8bf4e6937c94e4de891144
abbyssoul <abbyssoul@gmail.com>
2019-01-14 21:58:34 +1100

    Fix static init order issues

--- src/errorDomain.cpp
+++ src/errorDomain.cpp
@@ -36,12 +36,15 @@ static byte kKeysBuffer[kNbErrorCategories * sizeof(AtomValue)];
 static byte kValuesBuffer[kNbErrorCategories * sizeof(ErrorDomain*)];
 
 
-static auto kErrorDomainMap = makeDictionary<AtomValue, ErrorDomain const*>(
-    wrapMemory(kKeysBuffer), wrapMemory(kValuesBuffer)
-    );
+static Dictionary<AtomValue, ErrorDomain const*> kErrorDomainMap;
 
 
 uint32 Solace::registerErrorDomain(AtomValue categoryId, ErrorDomain const& domain) noexcept {
+    if (kErrorDomainMap.capacity() == 0) {
+        kErrorDomainMap = makeDictionary<AtomValue, ErrorDomain const*>(
+            wrapMemory(kKeysBuffer), wrapMemory(kValuesBuffer));
+    }
+
     kErrorDomainMap.put(categoryId, &domain);
 
     return kErrorDomainMap.size();
